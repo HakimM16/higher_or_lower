6c80fa930184b7a5da1da8d60557e32d64ce84d8
HakimM16 <hakimmabike2@gmail.com>
2025-11-16 21:16:12 +0000

    Changed the desk class by adding build_deck and deal_card functions

--- deck.py
+++ deck.py
@@ -7,20 +7,20 @@ class Deck():
     # Example: ('♠', 'Ace')
 
     def __init__(self):
-        self.deck = self.create_deck()
+        self.deck = self.build_deck() # acts as a stack
 
-    def create_deck(self):
-        # Create a standard deck of 10 cards to represent 10 rounds
-        deck = []
-        for _ in range(10):
-            card = Card()
-            # Check for duplicates before adding
-            while card.val in deck:
-                card = Card()
-            deck.append(card.val)
-        return deck
-
+    def build_deck(self):
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        self.deck = [Card(suit, rank) for suit in suits for rank in ranks]
+        random.shuffle(self.deck)
 
     def shuffle(self):
         # shuffle the deck using random.shuffle
         random.shuffle(self.deck)
+
+    def deal_card(self):
+        # deal a card from the top of the deck
+        if len(self.deck) == 0:
+            raise ValueError("No more cards in the deck")
+        return self.deck.pop()
